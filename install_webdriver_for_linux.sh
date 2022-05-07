9d3e2e9c8e8e2e7020a944e2eeda9e96cdc9ae22
Muhammad-Junaid-Jabbar <63024982+Muhammad-Junaid-Jabbar@users.noreply.github.com>
2022-05-07 14:19:32 +0500

    social_media_unique_username_checker files

---
+++ install_webdriver_for_linux.sh
@@ -0,0 +1,7 @@
+
+wget https://chromedriver.storage.googleapis.com/99.0.4844.51/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo cp chromedriver /usr/bin/chromedriver
+sudo chown root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
+sudo chmod 755 /usr/bin/chromedriver
